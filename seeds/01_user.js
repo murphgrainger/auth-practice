caa1c1e65a8fd221aa635bcd60a559e37822cd53
Murph Grainger <murph.grainger@gmail.com>
2017-01-26 10:12:49 -0700

    prepping for deployment

---
+++ seeds/01_user.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcrypt')
+
+exports.seed = function(knex, Promise) {
+    // Deletes ALL existing entries
+    return knex.raw('delete from "user"; ALTER SEQUENCE user_id_seq RESTART with 3;')
+        .then(function() {
+            const user = [{
+                id: 1,
+                name: 'Murph',
+                email: 'murph@info.com',
+                password: bcrypt.hashSync('password', 10),
+            }, {
+                id: 2,
+                name: 'Kiki',
+                email: 'kiki@info.com',
+                password: bcrypt.hashSync('password', 10),
+            }];
+            return knex('user').insert(user);
+        });
+};
